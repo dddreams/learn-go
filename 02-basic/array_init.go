b1733561a400e0efd38ca4c67056237316bc39b1
丁治恒 <289750872@qq.com>
2021-09-03 17:30:53 +0800

    GO语言学习

---
+++ 02-basic/array_init.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// 全局变量
+var arr0 [5]int = [5]int{1, 2, 3}
+var arr1 = [5]int{1, 2, 3, 4, 5}
+var arr2 = [...]int{1, 2, 3, 4, 5}
+var arr3 = [5]string{3: "hello", 4: "world"}
+
+func main() {
+	//局部变量
+	a := [3]int{1, 2}           // 未初始化元素值为 0。
+	b := [...]int{1, 2, 3, 4}   // 通过初始化值确定数组长度。
+	c := [5]int{2: 100, 4: 200} // 使用引号初始化元素。
+	d := [...]struct {
+		name string
+		age  uint8
+	}{
+		{"user1", 10}, // 可省略元素类型。
+		{"user2", 20}, // 别忘了最后一行的逗号。
+	}
+	fmt.Println(arr0)
+	fmt.Println(arr3)
+	fmt.Println(a, b, c, d)
+}
